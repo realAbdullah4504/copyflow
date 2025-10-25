2385d6bdf72bf1b0daca0641fb0984639fe3cf51
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-10-25 18:44:52 +0500

    build done

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -1,9 +1,7 @@
-"use client"
-
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
-import { cn } from ""@/utils""
+import { cn } from "@/utils"
 import { buttonVariants } from "@/components/ui/button"
 
 const AlertDialog = AlertDialogPrimitive.Root
