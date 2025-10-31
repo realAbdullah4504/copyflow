3141034022237da4a8af08fab0db56d3d8d268f9
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-10-31 16:29:21 +0500

    submissionPage admin

--- src/pages/dashboard/admin/SubmissionsPage.tsx
+++ src/pages/dashboard/admin/SubmissionsPage.tsx
@@ -5,9 +5,23 @@ import {
   SubmissionTable,
 } from "@/components/submissions";
 import { useAllSubmissions } from "@/hooks/queries";
+import { useTableParams } from "@/hooks";
 
 const SubmissionPage = () => {
-  const { submissions } = useAllSubmissions();
+  const {
+      pagination,
+      setPagination,
+      filters,
+      columnFilters,
+      setColumnFilters,
+      sorting,
+      setSorting,
+    } = useTableParams();
+  const { submissions, total, isLoading } = useAllSubmissions({
+    pagination,
+    filters,
+    sorting,
+  });
 
 
   const columns = getSubmissionColumns(ROLES.ADMIN);
@@ -17,7 +31,21 @@ const SubmissionPage = () => {
         title="All Submissions"
         role={ROLES.ADMIN}
       />
-      <SubmissionTable data={submissions} columns={columns} />
+      <SubmissionTable
+        data={submissions}
+        columns={columns}
+        pagination={pagination}
+        columnFilters={columnFilters}
+        sorting={sorting}
+        total={total}
+        isLoading={isLoading}
+        onPaginationChange={setPagination}
+        onColumnFiltersChange={setColumnFilters}
+        onSortingChange={setSorting}
+        showFilters
+        showPagination
+        showSorting
+      />
     </>
   );
 };
